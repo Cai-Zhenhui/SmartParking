0bcc0e6368ba76043b75d50ffd3e3d9209def54a
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-09-25 22:41:01 +0800

    fix upload

--- Client/uploadFile.py
+++ Client/uploadFile.py
@@ -1,11 +1,10 @@
 import os
 import socket
 class Client:
-    self.IP_PORT
     def Client(selt):
-        self.IP_PORT=("47.93.148.239",2222)
-        self.socket=socket.socket()
-        self.socket.connect(self.IP_PORT)
+        IP_PORT=("47.93.148.239",2222)
+        sk=socket.socket()
+        sk.connect(IP_PORT)
         #self.currentDir=os.path.dirname(os.path.abspath(__file__))
         pass
 
