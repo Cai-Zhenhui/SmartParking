37207c9eecbb457c905c37103da8dd70adedd0f7
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-09-26 16:06:29 +0800

    add server

--- Client/uploadFile.py
+++ Client/uploadFile.py
@@ -1,20 +1,32 @@
 import os
 import socket
+IP="47.93.148.239"
+PORT=2222
+BUFFER_SIZE=1024
 class Client:
     def __init__(self):
-        #self.IP_PORT=("47.93.148.239",2222)
-        #self.socket=socket.socket()
-        #self.socket.connect(self.IP_PORT)
+        self.socket=socket.socket()
+        self.socket.connect((IP,PORT))
         self.currentDir=os.path.dirname(os.path.abspath(__file__))
         pass
+    def __del__(self):
+        self.socket.close()
+    pass
     def sendFile(self,filePath):
         path=os.path.join(self.currentDir,filePath)
-        fileName=os.path.basename(filePath)
-        fileSize=os.stat(filePath).st_size
+        fileName=os.path.basename(path)
+        fileSize=os.stat(path).st_size
         fileInfo="post|%s|%s"%(fileName,fileSize)
         print(fileInfo)
+        pass
+    def sendString(self,str):
+        data=input(">:")
+        self.socket.send(data.encode())
+        data=self.socket.recv(BUFFER_SIZE)
+        print(data.encode())
         pass
     pass
 
 c=Client()
 c.sendFile("."+os.path.sep+"uploadFile.py")
+c.sendString("Hello,World!")
---
+++ Server/server.py
@@ -0,0 +1,30 @@
+import os
+import socket
+import time
+IP="127.0.0.1"
+PORT=2222
+BUFFER_SIZE=1024
+class Server:
+    def __init__(self):
+        self.socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.socket.bind((IP,PORT))
+        self.socket.listen(1)
+        print("Server is running! Waiting for client!")
+        self.clientSocket,self.clientAddr=self.socket.accept()
+        print("Connect Addr:",self.clientAddr)
+        pass
+    def __del__(self):
+        self.socket.close()
+        pass
+    def run(self):
+        while(True):
+            data=self.clientSocket.recv(BUFFER_SIZE)
+            print(data)
+            self.clientSocket.send(('[%s] %s' % (time.ctime(), data)).encode())
+            pass
+        pass
+    pass
+if __name__ == "__main__":
+    server=Server()
+    server.run()
+    pass
