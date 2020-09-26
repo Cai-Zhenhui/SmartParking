661f086a4a8c9d492c6e5566d867c579dd00707e
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-09-26 20:14:29 +0800

    ff

--- Server/testFR.py
+++ Server/testFR.py
@@ -2,6 +2,7 @@ import face_recognition as fr
 import cv2
 import os
 print(os.path.abspath(os.path.curdir))
-img=fr.load_image_file(os.path.abspath(os.path.curdir)+os.path.sep+"Client"+os.path.sep+"test.bmp")
+#img=fr.load_image_file(os.path.abspath(os.path.curdir)+os.path.sep+"Client"+os.path.sep+"test.bmp")
+img=fr.load_image_file(os.path.abspath(os.path.curdir)+os.path.sep+"test.bmp")
 print(fr.face_locations(img))
 print(fr.face_encodings(img))
