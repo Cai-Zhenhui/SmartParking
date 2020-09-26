60cfba02790055ccd464278a5d1d1dca693910b0
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-09-26 15:24:11 +0800

    fix upload

--- Client/uploadFile.py
+++ Client/uploadFile.py
@@ -1,15 +1,14 @@
 import os
 import socket
 class Client:
-    def Client(selt):
-        IP_PORT=("47.93.148.239",2222)
-        sk=socket.socket()
-        sk.connect(IP_PORT)
-        #self.currentDir=os.path.dirname(os.path.abspath(__file__))
+    def __init__(self):
+        #self.IP_PORT=("47.93.148.239",2222)
+        #self.socket=socket.socket()
+        #self.socket.connect(self.IP_PORT)
+        self.currentDir=os.path.dirname(os.path.abspath(__file__))
         pass
-
-    def sendFile(selt,filePath):
-        #path=os.path.join(self.currentDir,path)
+    def sendFile(self,filePath):
+        path=os.path.join(self.currentDir,filePath)
         fileName=os.path.basename(filePath)
         fileSize=os.stat(filePath).st_size
         fileInfo="post|%s|%s"%(fileName,fileSize)
@@ -18,4 +17,4 @@ class Client:
     pass
 
 c=Client()
-c.sendFile(".\\uploadFile.py")
+c.sendFile("."+os.path.sep+"uploadFile.py")
