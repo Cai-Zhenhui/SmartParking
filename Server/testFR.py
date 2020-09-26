21178780f5984775b3affb15d2080a1e9ceeb6a7
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-09-26 20:09:36 +0800

    fix add

--- Server/testFR.py
+++ Server/testFR.py
@@ -1,5 +1,7 @@
 import face_recognition as fr
 import cv2
-img=fr.load_image_file("test.bmp")
+import os
+print(os.path.abspath(os.path.curdir))
+img=fr.load_image_file(os.path.abspath(os.path.curdir)+os.path.sep+"Client"+os.path.sep+"test.bmp")
 print(fr.face_locations(img))
 print(fr.face_encodings(img))
