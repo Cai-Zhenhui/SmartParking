de30f9ba87c46ddb9ff1cc6a133b41cec1c3f813
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-09-27 17:35:41 +0800

    try send file

---
+++ Client/uploadFile2.py
@@ -0,0 +1,88 @@
+# -*- coding: UTF-8 -*-
+import os
+import socket
+import time
+IP="47.93.148.239"
+PORT=2222
+BUFFER_SIZE=1024
+FLAG_HEAD_RECV=1
+FLAG_FILE_RECV=2
+class Client:
+    def __init__(self):
+        self.socket=socket.socket()
+        self.socket.connect((IP,PORT))
+        self.currentDir=os.path.dirname(os.path.abspath(__file__))
+        pass
+    def __del__(self):
+        self.socket.close()
+    pass
+    def sendFile(self,filePath):
+        path=os.path.join(self.currentDir,filePath)
+        fileName=os.path.basename(path)
+        fileSize=os.stat(path).st_size
+        #构造文件信息
+        #post|name|字节大小
+        fileInfo="LP|%s|%s"%(fileName,fileSize)
+        print(fileInfo)
+
+        #发送文件头长度 该部分不超过3字节
+        self.socket.send(("%03d"%(len(fileInfo))).encode('utf-8'))
+
+        #发送文件信息
+        self.socket.send(fileInfo.encode('utf-8'))
+        #接受 服务端反馈
+        tempBuffer=self.socket.recv(1)
+        tempTime=int(time.time())
+        while not tempBuffer:
+            if int(time.time())-tempTime>30:
+                print("timeout!")
+                return
+            tempBuffer=self.socket.recv(1)
+        tempBuffer=tempBuffer.decode()
+        if int(tempBuffer)!=FLAG_HEAD_RECV:
+            print("ERROR FLAG_HEAD_RECV")
+            pass
+
+        #收到反馈 发送文件实体
+        with open(path,"rb") as target:
+            data=target.read()
+            self.socket.sendall(data)
+            while(not data):
+                data=target.read(BUFFER_SIZE)
+                self.socket.send(data)
+                pass
+            pass
+        #接受标志
+        tempBuffer=self.socket.recv(1)
+        tempTime=int(time.time())
+        while not tempBuffer:
+            if int(time.time())-tempTime>30:
+                print("timeout!")
+                return
+            tempBuffer=self.socket.recv(1)
+        tempBuffer=tempBuffer.decode()
+        if int(tempBuffer)!=FLAG_FILE_RECV:
+            print("ERROR FLAG_FILE_RECV")
+            pass
+        #接受返回信息
+        tempBuffer=self.socket.recv(BUFFER_SIZE)
+        tempTime=int(time.time())
+        while not tempBuffer:
+            if int(time.time())-tempTime>30:
+                print("timeout!")
+                return
+            tempBuffer=self.socket.recv(BUFFER_SIZE)
+        tempBuffer=tempBuffer.decode()
+
+        pass
+    def sendString(self,str):
+        data=input(">:")
+        self.socket.send(data.encode())
+        data=self.socket.recv(BUFFER_SIZE)
+        print(data)
+        pass
+    pass
+if __name__ == "__main__":
+    c=Client()
+    c.sendFile(input("input fileName:"))
+    pass
---
+++ Server/server2.py
@@ -0,0 +1,119 @@
+# -*- coding: UTF-8 -*-
+import os
+import socket
+import time
+import threading
+import dealPL
+IP='0.0.0.0'
+PORT=2222
+BUFFER_SIZE=1024
+FLAG_HEAD_RECV=1
+FLAG_FILE_RECV=2
+class Server:
+    def __init__(self):
+        #self.imgPath="."+os.path.sep+"Server"+os.path.sep+"upload"+os.path.sep
+        self.c = dealPL.CardPredictor()
+        self.c.train_svm()
+        
+        self.socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.socket.bind((IP,PORT))
+        self.socket.listen(1)
+        self.isExit=False
+        print("Server is running! Waiting for client!")
+        pass
+    def __del__(self):
+        self.clientSocket.close()
+        self.socket.close()
+        pass
+    def run(self):
+        self.clientSocket,self.clientAddr=self.socket.accept()
+        print("Connect Addr:",self.clientAddr)
+        self.linkTime=int(time.time())
+        while(not self.isExit):
+            temp=self.clientSocket.recv(3)
+            print(temp)
+            sizeInfo=int(temp.decode())
+            fileInfo=self.clientSocket.recv(sizeInfo)
+            print(fileInfo)
+            if not fileInfo:
+                time.sleep(1)
+                continue
+            print(fileInfo)
+            infoList=fileInfo.decode().split("|")
+            print(infoList)
+            print("!!!!!!!!!!!")
+            imgType=""#face or LP
+            fileName=""
+            fileSize=0
+            if len(infoList)==3:
+                #接收到头信息
+                imgType=str(infoList[0])
+                fileName=str(infoList[1])
+                fileSize=int(infoList[2])
+                #发送头信息接受完成标志
+                self.clientSocket.send(str(FLAG_HEAD_RECV).encode())
+                
+                buffer=b''
+                totalData=b''
+                size=0
+                #准备接受数据
+                buffer=self.clientSocket.recv(BUFFER_SIZE)
+                size+=BUFFER_SIZE
+                totalData+=buffer
+                while(len(totalData)<fileSize):
+                    buffer=self.clientSocket.recv(BUFFER_SIZE)
+                    size+=BUFFER_SIZE
+                    totalData+=buffer
+                    pass#end while(len(totalData)<fileSize):
+
+                with open(fileName,"wb") as target:
+                    target.write(totalData)
+                    pass
+                print("接受完毕")
+
+                #发送接收完毕标志
+                self.clientSocket.send(str(FLAG_FILE_RECV).encode())
+                if imgType=="Face":
+                    print("将处理 FACE")
+                    pass
+                elif imgType=="LP":
+                    print("将处理 LP")
+                    r, roi, color = self.c.predict(fileName)
+                    print(r,roi,color)
+                    lp=""
+                    for i in range(len(r)):
+                        lp=lp+r[i]
+                        pass
+                    if len(r)<6:
+                        self.clientSocket.send(lp.encode("utf-8"))
+                        pass
+                    else:
+                        self.clientSocket.send("NULL".encode("utf-8"))
+                        pass
+                    pass#end elif imgType=="LP":
+                pass#end if len(infoList)==3:
+            pass
+        pass
+    pass
+
+class myThread (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.server=Server()
+        pass
+    def __del__(self):
+        pass
+    #执行myThread.start()时会调用该函数
+    def run(self):
+        print("socketThread执行")
+        self.server.run()
+        print("socketThread退出")
+        pass
+    pass
+if __name__ == "__main__":
+    socketThread=myThread()
+    socketThread.start()
+    a=input("input 'q' exit:")
+    socketThread.server.isExit=True
+    socketThread.join()
+    pass
