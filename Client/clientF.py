0ebcfb624b163e2245f88bde521a92fb14decb0c
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-09-29 10:36:33 +0800

    fix server

--- Client/clientF.py
+++ Client/clientF.py
@@ -15,19 +15,23 @@ while True:
         break
     elif choose==ord('g'):
         #检测到用户输入 准备人脸识别取车
+        '''
         gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         rect=cascader.detectMultiScale(gray,1.3,5)
         for x,y,w,h in rect:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),10)
+            pass'''
+        cv2.imwrite("tmp.jpg",frame)
+
+        rects=eval(c.sendFile("tmp.jpg",True))
+        if len(rects)!=0:
+            for t,r,b,l in rects:
+                cv2.rectangle(frame,(l,t),(r,b),(255,0,0),10)
+                pass
             pass
         pass#end elif choose==ord('g'):
     cv2.imwrite("tmp.jpg",frame)
-    
-    rects=eval(c.sendFile("tmp.jpg",True))
-    if len(rects)!=0:
-        for t,r,b,l in rects:
-            cv2.rectangle(frame,(l,t),(r,b),(255,0,0),10)
-            pass
-        pass
+    rects=c.sendFile("tmp.jpg",False)
+    print(rects)
     cv2.imshow("camera",frame)
     pass
