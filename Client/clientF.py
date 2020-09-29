0320688b8015582bf7ce771048cbac3f804667fd
Cai-Zhenhui <20174698@stu.neu.edu.cn>
2020-09-29 15:54:43 +0800

    11

--- Client/clientF.py
+++ Client/clientF.py
@@ -25,7 +25,7 @@ while True:
         ret=c.sendFile("tmp.jpg",True)
         if ret!="NULL":
             w,h=frame.shape[:2]
-            cv2.putText(frame, "已找到你的汽车："+ret, (w/2, h/2), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, ))
+            cv2.putText(frame, "已找到你的汽车："+ret, (int(w/2), int(h/2)), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, ))
             #绘制结果
             pass
         input()
@@ -35,13 +35,15 @@ while True:
     ret=c.sendFile("tmp.jpg",False)
     if ret!="NULL":
         w,h=frame.shape[:2]
+        cv2.putText(frame, ret+" 当前时间"+time.ctime(), (int(w/2), h), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, ))
+        '''
         if ret.find("8")!=-1 or ret.find("5")!=-1 or ret.find("V")!=-1:
             #苏E 05EV8
-            cv2.putText(frame, "苏E05EV8 当前时间"+time.ctime(), (w/2, h), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, ))
+            cv2.putText(frame, "苏E05EV8 当前时间"+time.ctime(), (int(w/2), h), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, ))
             pass
         elif ret.find("A")!=-1 or ret.find("1")!=-1 or ret.find("2")!=-1:
-            cv2.putText(frame, "辽A0AE12 当前时间"+time.ctime(), (w/2, h), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, ))
-            pass
+            cv2.putText(frame, "辽A0AE12 当前时间"+time.ctime(), (int(w/2), h), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, ))
+            pass'''
         pass
     print(ret)
     cv2.imshow("camera",frame)
